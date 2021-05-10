597eb33ceb97cc596f6b464b69fb141f5bb015f5
dmircheski <deni.mirceski@t.mk>
2021-05-11 01:35:18 +0200

    authenticate initially set

--- authServer.js
+++ authServer.js
@@ -3,10 +3,42 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 app.use(express.json())
 
 let refreshTokens = []
+let users = []
+
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.username === req.body.username)
+    if (user == null) {
+      return res.status(400).send('Cannot find user')
+    }
+    try {
+      if(await bcrypt.compare(req.body.password, user.password)) {
+        res.send('Success')
+      } else {
+        res.send('Not Allowed')
+      }
+    } catch {
+      res.status(500).send()
+    }
+
+    // const username = req.body.username
+    // const user = { name: username }
+
+    // const accessToken = generateAccessToken(user)
+    // const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    // refreshTokens.push(refreshToken)
+    // res.json({ accessToken: accessToken, refreshToken: refreshToken })
+
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
 
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token
@@ -19,22 +51,19 @@ app.post('/token', (req, res) => {
     })
 })
 
-app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
-app.post('/login', (req, res) => {
-    // Authenticate User
-
-    const username = req.body.username
-    const user = { name: username }
-
-    const accessToken = generateAccessToken(user)
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken)
-    res.json({ accessToken: accessToken, refreshToken: refreshToken })
-
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { username: req.body.username, password: hashedPassword }
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
 })
 
 function generateAccessToken(user) {
