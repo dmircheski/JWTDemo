46133ffb38cb7e8c1ae9cf2639b2b75e3ca66e85
dmircheski <deni.mirceski@t.mk>
2021-05-11 02:48:14 +0200

    Last commit, everything done

--- authServer.js
+++ authServer.js
@@ -11,63 +11,66 @@ let refreshTokens = []
 let users = []
 
 app.post('/login', async (req, res) => {
-    const user = users.find(user => user.username === req.body.username)
-    if (user == null) {
-      return res.status(400).send('Cannot find user')
+  const user = users.find(user => user.username === req.body.username)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const accessToken = generateAccessToken(user)
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+      refreshTokens.push(refreshToken)
+      res.json({ accessToken: accessToken, refreshToken: refreshToken })
+    } else {
+      res.send('Not Allowed')
     }
-    try {
-      if(await bcrypt.compare(req.body.password, user.password)) {
-        res.send('Success')
-      } else {
-        res.send('Not Allowed')
-      }
-    } catch {
-      res.status(500).send()
-    }
+  } catch {
+    res.status(500).send()
+  }
 
-    // const username = req.body.username
-    // const user = { name: username }
+  // const username = req.body.username
+  // const user = { name: username }
 
-    // const accessToken = generateAccessToken(user)
-    // const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    // refreshTokens.push(refreshToken)
-    // res.json({ accessToken: accessToken, refreshToken: refreshToken })
+  // const accessToken = generateAccessToken(user)
+  // const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  // refreshTokens.push(refreshToken)
+  // res.json({ accessToken: accessToken, refreshToken: refreshToken })
 
 })
 
 app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
 })
 
 app.post('/token', (req, res) => {
-    const refreshToken = req.body.token
-    if(refreshToken == null) return res.sendStatus(401)
-    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403)
-        const accessToken = generateAccessToken({ name: user.name })
-        res.json({ accessToken: accessToken})
-    })
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
 })
 
 app.get('/users', (req, res) => {
-    res.json(users)
+  res.json(users)
 })
 
 app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = { username: req.body.username, password: hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { username: req.body.username, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
 })
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
-}  
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
 
 app.listen(4000)
